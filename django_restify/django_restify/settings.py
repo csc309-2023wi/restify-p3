5e32b1f2654268ec081b4f7e0013bb3a6241eec4
Samm Du <hmu@sammdu.com>
2023-03-07 23:19:45 -0500

    defined Property and Reservation models; registered custom User

--- django_restify/django_restify/settings.py
+++ django_restify/django_restify/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # custom configuration
     "rest_framework",
+    "api.apps.ApiConfig",
 ]
 
 MIDDLEWARE = [
@@ -99,6 +101,9 @@ AUTH_PASSWORD_VALIDATORS = [
         "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
+
+# register custom user model
+AUTH_USER_MODEL = "api.User"
 
 
 # Internationalization
