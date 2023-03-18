da2e89d9486524d8cc674bf116ca87480a7368e7
shawnpl7 <90327099+shawnpl7@users.noreply.github.com>
2023-03-17 21:05:32 -0400

    Remove bad auth setting

--- django_restify/django_restify/settings.py
+++ django_restify/django_restify/settings.py
@@ -132,7 +132,6 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.AllowAny',
-        'rest_framework.permissions.IsAuthenticated',
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': (
       'rest_framework_simplejwt.authentication.JWTAuthentication',
