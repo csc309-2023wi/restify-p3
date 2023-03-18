c7ee462b917ecc00f629ee75ad447a21449b60c7
Samm Du <samm.du@mail.utoronto.ca>
2023-03-17 20:06:23 -0400

    Property Views Implementation (#3)
    * property views implementation initial success
    
    * added sorting by availability and fake rating

--- django_restify/api/urls.py
+++ django_restify/api/urls.py
@@ -6,7 +6,12 @@ urlpatterns = [
     # path("comment/", views.__.as_view(), name="comment"),
     # path("image/", views.__.as_view(), name="image"),
     # path("notification/", views.__.as_view(), name="notification"),
-    # path("property/", views.__.as_view(), name="property"),
+    path("property/", views.PropertyListCreateView.as_view(), name="property"),
+    path(
+        "property/<int:id>",
+        views.PropertyRetrieveUpdateDestroyView.as_view(),
+        name="property_one",
+    ),
     # path("reservation/", views.__.as_view(), name="reservation"),
     # path("user/", views.__.as_view(), name="user"),
 ]
