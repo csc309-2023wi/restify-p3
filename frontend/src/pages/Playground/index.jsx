ab9fddcee5fc1989adc65e016059dab306ed73bd
Samm Du <hmu@sammdu.com>
2023-04-03 18:37:23 -0400

    added playground display

--- frontend/src/pages/Playground/index.jsx
+++ frontend/src/pages/Playground/index.jsx
@@ -7,9 +7,57 @@ function Playground() {
     return (
         <>
             <h1>Playground</h1>
-            <p>Insert random components in this page for experimentation. </p>
-            <Sidebar />
-            <ActionBtn className={"gray-dark"} text="Press Me"/>
+            <p style={{ marginBottom: "2rem" }}>Insert random components in this page for experimentation. </p>
+            <div
+                style={{
+                    display: "flex",
+                    flexDirection: "row",
+                    rowGap: "1rem",
+                }}>
+                <Sidebar />
+                <div>
+                    <ActionButtons />
+                </div>
+            </div>
+        </>
+    );
+}
+
+function ActionButtons() {
+    return (
+        <>
+            <ul
+                style={{
+                    display: "flex",
+                    flexDirection: "row",
+                    flexWrap: "wrap",
+                    columnGap: "1rem",
+                    rowGap: "1rem",
+                }}>
+                <li>
+                    <ActionBtn className={"gray-dark"} text="Press Me" />
+                </li>
+                <li>
+                    <ActionBtn className={"purple-dark"} text="Press Me" />
+                </li>
+                <li>
+                    <ActionBtn className={"green-dark"} text="Press Me" />
+                </li>
+                <li>
+                    <ActionBtn className={"purple-light"} text="Press Me" />
+                </li>
+                <li>
+                    <ActionBtn className={"green-light"} text="Press Me" />
+                </li>
+            </ul>
+            <div
+                style={{
+                    backgroundColor: "var(--clr-purple-primary)",
+                    padding: "1rem",
+                    margin: "1rem 0",
+                }}>
+                <ActionBtn className={"bordered-dark"} text="Press Me" />
+            </div>
         </>
     );
 }
