603a53507f1749aacd792a4f075b03458d870aca
Samm Du <hmu@sammdu.com>
2023-04-21 14:34:03 -0400

    completed generic modal

---
+++ frontend/src/components/Modal/index.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import "./modal.css";
+
+// import icons
+import xWhite from "../../assets/icons/x-white.svg";
+import checkWhite from "../../assets/icons/check-white.svg";
+
+function Modal({
+    id,
+    modalHeader,
+    displayState,
+    displayStateSetter,
+    mainImageContent,
+    mainInfoContent,
+    createNewAction,
+    actionContent,
+}) {
+    const displayAttr = displayState ? "flex" : "none";
+    const imageSection = mainImageContent ? <div className="image-section">{mainImageContent}</div> : null;
+    const infoSection = mainInfoContent ? <div className="info-section">{mainInfoContent}</div> : null;
+    const createNewButton = createNewAction ? (
+        <button className="action-btn create-listing green-dark" onClick={createNewAction}>
+            <img src={checkWhite} alt="+" />
+        </button>
+    ) : null;
+    return (
+        <div className="modal-backdrop" id={id} key={id} style={{ display: displayAttr }}>
+            <div className="modal-container">
+                <section className="modal-content">
+                    {imageSection}
+                    {infoSection}
+                </section>
+
+                <aside className="modal-action">
+                    <button className="btn-modal-close clickable-on-dark" onClick={() => displayStateSetter(false)}>
+                        <img src={xWhite} alt="X" />
+                    </button>
+                    {createNewButton}
+                    <div className="modal-header">
+                        <h3>{modalHeader}</h3>
+                    </div>
+                    <div className="item-container">{actionContent}</div>
+                </aside>
+            </div>
+        </div>
+    );
+}
+
+export default Modal;
--- frontend/src/pages/Playground/index.jsx
+++ frontend/src/pages/Playground/index.jsx
@@ -1,16 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import ActionBtn from "../../components/ActionBtn";
 import Sidebar from "../../components/Sidebar";
 import Navbar from "../../components/Navbar";
+import Modal from "../../components/Modal";
+
 import "./playground.css";
 
 function Playground() {
+    const [newModalShow, setNewModalShow] = useState(true);
     return (
         <>
             <Navbar />
             <div className="container">
                 <h1>Playground</h1>
                 <p style={{ marginBottom: "2rem" }}>Insert random components in this page for experimentation. </p>
+
                 <div
                     style={{
                         display: "flex",
@@ -19,7 +23,28 @@ function Playground() {
                     }}>
                     <Sidebar />
                     <div>
+                        {/* DEMO: Action Buttons */}
                         <ActionButtons />
+
+                        {/* DEMO: Modals */}
+                        <div className="modal-demos" style={{ margin: "2rem 0 1rem 0" }}>
+                            <h2 style={{ marginBottom: "1rem" }}>Modals demo</h2>
+                            <Modal
+                                id={"new"}
+                                modalHeader={"Header of the modal"}
+                                displayState={newModalShow} // bool: whether the modal should be shown
+                                displayStateSetter={setNewModalShow} // function that sets whether the modal should be shown
+                                mainImageContent={<h4>Image section</h4>} // content to put inside the image section on the left; put null to disable
+                                mainInfoContent={<h4>Info section</h4>} // content to put inside the info section on the left; put null to disable
+                                createNewAction={(event) => console.log("Submit!", event.currentTarget)} // what happens when you click the green submit button; put null to disable
+                                actionContent={<h4>Action column</h4>} // content to put inside the action column on the right; put null to disable
+                            />
+                            <ActionBtn
+                                className={"green-dark"}
+                                text="Show new modal"
+                                onClick={() => setNewModalShow(true)}
+                            />
+                        </div>
                     </div>
                 </div>
             </div>
