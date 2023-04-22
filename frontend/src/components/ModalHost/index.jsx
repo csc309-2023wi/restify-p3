cff1ee6b8fa6e7cc1c653d60df6736738c9e55d5
Samm Du <hmu@sammdu.com>
2023-04-22 02:08:56 -0400

    started migrating host create form

--- frontend/src/components/ModalHost/index.jsx
+++ frontend/src/components/ModalHost/index.jsx
@@ -1,12 +1,80 @@
 import React, { useState, useEffect } from "react";
 import "./modal_host.css";
 
+// import icons
+import xDark from "../../assets/icons/x-dark.svg";
+
 // import components
 import Modal from "../Modal";
 import PropertyImageSelector from "./propertyImageSelector";
+import Input from "../Input";
+import LocationInput from "../LocationInput";
+import GuestInput from "../GuestInput";
+import DateInput from "../DateInput";
+import PriceInput from "../PriceInput";
 
 export function ModalHostCreate({ displayState, displayStateSetter }) {
-    const actionContent = "action";
+    const actionContent = (
+        <>
+            <div className="input-container">
+                <h4>Address:</h4>
+                <Input inputBody={<LocationInput />} appendClassNames={"default"} />
+            </div>
+
+            <div className="input-container">
+                <h4>Number of guests allowed:</h4>
+                <Input inputBody={<GuestInput />} appendClassNames={"guests"} />
+            </div>
+
+            <div className="input-container">
+                <h4>Dates Available:</h4>
+                <div>
+                    <ul className="content-list">
+                        <li className="availability-item card pos-relative">
+                            <button className="action-btn del-item">
+                                <img src={xDark} alt="x" />
+                            </button>
+                            <p>
+                                From: <span className="date">March 1, 2025</span>
+                            </p>
+                            <p>
+                                To: <span className="date">March 1, 2026</span>
+                            </p>
+                            <p>
+                                Price: <span className="price">$500</span>
+                            </p>
+                        </li>
+
+                        <li className="availability-item card pos-relative">
+                            <button className="action-btn del-item">
+                                <img src={xDark} alt="x" />
+                            </button>
+                            <p>
+                                From: <span className="date">March 1, 2025</span>
+                            </p>
+                            <p>
+                                To: <span className="date">March 1, 2026</span>
+                            </p>
+                            <p>
+                                Price: <span className="price">$500</span>
+                            </p>
+                        </li>
+                    </ul>
+                    <div className="add-availability">
+                        <Input inputBody={<DateInput dateLabel="FROM" appendClassNames={"dates-from"} />} />
+                        <Input inputBody={<DateInput dateLabel="TO" />} appendClassNames={"default"} />
+                        <Input inputBody={<PriceInput />} appendClassNames={"default"} />
+                        <button className="action-btn purple-dark">Add Availability</button>
+                    </div>
+                </div>
+            </div>
+
+            <div className="input-container">
+                <h4>Amenities:</h4>
+                <div className="amenity-container">{/* TODO: add amenities */}</div>
+            </div>
+        </>
+    );
 
     return (
         <Modal
