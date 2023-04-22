49e60319009a4ef65f0a189eb1da70ef59605e25
Samm Du <hmu@sammdu.com>
2023-04-22 01:20:39 -0400

    refactor PropertyImageSelector

--- frontend/src/components/ModalHost/index.jsx
+++ frontend/src/components/ModalHost/index.jsx
@@ -1,12 +1,9 @@
 import React, { useState, useEffect } from "react";
 import "./modal_host.css";
 
-// import icons
-import xSmallWhite from "../../assets/icons/x-white-small.svg";
-
 // import components
 import Modal from "../Modal";
-import AddImageRegion from "../AddImageRegion";
+import PropertyImageSelector from "./propertyImageSelector";
 
 export function ModalHostCreate({ displayState, displayStateSetter }) {
     const actionContent = "action";
@@ -17,69 +14,12 @@ export function ModalHostCreate({ displayState, displayStateSetter }) {
             modalHeader={"Create new listing"}
             displayState={displayState} // bool: whether the modal should be shown
             displayStateSetter={displayStateSetter} // function that sets whether the modal should be shown
-            mainImageContent={<PropertyImageUploader />} // content to put inside the image section on the left; put null to disable
+            mainImageContent={<PropertyImageSelector />} // content to put inside the image section on the left; put null to disable
             mainInfoContent={null} // content to put inside the info section on the left; put null to disable
             createNewAction={(event) => console.log("Host create!", event.currentTarget)} // what happens when you click the green submit button; put null to disable
             actionContent={actionContent} // content to put inside the action column on the right; put null to disable
         />
     );
-}
-
-function PropertyImageUploader() {
-    const [images, setImages] = useState([]);
-
-    const fileHandler = (file) => {
-        const localPreviewURL = URL.createObjectURL(file);
-        const fileExtension = file.name.match(/^.+(\.[^\.]+)$/)[1].split(".")[1];
-        fileToDataURL(file).then((dataURL) => {
-            const base64Rep = dataURL.split(",")[1];
-            const newImageObject = {
-                file: file,
-                fileExtension: fileExtension,
-                base64Rep: base64Rep,
-                previewURL: localPreviewURL,
-            };
-            // console.log(newImageObject);
-            setImages((prevImages) => [...prevImages, newImageObject]);
-        });
-    };
-
-    const handleDeleteImage = (index) => {
-        setImages((prevImages) => {
-            const newImages = [...prevImages];
-            newImages.splice(index, 1);
-            return newImages;
-        });
-    };
-
-    const imagePreviews = (
-        <>
-            {images.map((image, index) => (
-                <div className="pos-relative" key={"upload_" + index}>
-                    <button className="del-img clickable-on-dark" onClick={() => handleDeleteImage(index)}>
-                        <img src={xSmallWhite} alt="x" />
-                    </button>
-                    <img className="propery-img" src={image.previewURL} alt={`property image ${index}`} />
-                </div>
-            ))}
-        </>
-    );
-
-    return (
-        <div className="image-selector">
-            {imagePreviews}
-            <AddImageRegion fileHandler={fileHandler} />
-        </div>
-    );
-}
-
-function fileToDataURL(file) {
-    return new Promise((resolve, reject) => {
-        let reader = new FileReader();
-        reader.onload = () => resolve(reader.result);
-        reader.onerror = reject;
-        reader.readAsDataURL(file);
-    });
 }
 
 export function ModalHostExisting({ property_id, displayState, displayStateSetter }) {
---
+++ frontend/src/components/ModalHost/propertyImageSelector.jsx
@@ -0,0 +1,66 @@
+import { useState } from "react";
+
+// import icons
+import xSmallWhite from "../../assets/icons/x-white-small.svg";
+
+// import components
+import AddImageRegion from "../AddImageRegion";
+
+function PropertyImageSelector() {
+    const [images, setImages] = useState([]);
+
+    const fileHandler = (file) => {
+        const localPreviewURL = URL.createObjectURL(file);
+        const fileExtension = file.name.match(/^.+(\.[^\.]+)$/)[1].split(".")[1];
+        fileToDataURL(file).then((dataURL) => {
+            const base64Rep = dataURL.split(",")[1];
+            const newImageObject = {
+                file: file,
+                fileExtension: fileExtension,
+                base64Rep: base64Rep,
+                previewURL: localPreviewURL,
+            };
+            // console.log(newImageObject);
+            setImages((prevImages) => [...prevImages, newImageObject]);
+        });
+    };
+
+    const handleDeleteImage = (index) => {
+        setImages((prevImages) => {
+            const newImages = [...prevImages];
+            newImages.splice(index, 1);
+            return newImages;
+        });
+    };
+
+    const imagePreviews = (
+        <>
+            {images.map((image, index) => (
+                <div className="pos-relative" key={"upload_" + index}>
+                    <button className="del-img clickable-on-dark" onClick={() => handleDeleteImage(index)}>
+                        <img src={xSmallWhite} alt="x" />
+                    </button>
+                    <img className="propery-img" src={image.previewURL} alt={`property image ${index}`} />
+                </div>
+            ))}
+        </>
+    );
+
+    return (
+        <div className="image-selector">
+            {imagePreviews}
+            <AddImageRegion fileHandler={fileHandler} />
+        </div>
+    );
+}
+
+function fileToDataURL(file) {
+    return new Promise((resolve, reject) => {
+        let reader = new FileReader();
+        reader.onload = () => resolve(reader.result);
+        reader.onerror = reject;
+        reader.readAsDataURL(file);
+    });
+}
+
+export default PropertyImageSelector;
