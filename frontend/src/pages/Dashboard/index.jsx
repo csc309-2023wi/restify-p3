0e4b85d9f793f0f91ece1c0156bbb1f23e8dc0af
Samm Du <hmu@sammdu.com>
2023-04-23 04:49:16 -0400

    removed unused code and imports; read token from localStorage; autoformatting

--- frontend/src/pages/Dashboard/index.jsx
+++ frontend/src/pages/Dashboard/index.jsx
@@ -1,9 +1,9 @@
-import React, { useState, useEffect, useContext } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect, useContext } from "react";
+// import axios from "axios";
 import "./dashboard.css";
 import Navbar from "../../components/Navbar";
-import { useNavigate } from 'react-router-dom';
-import { HomeContext } from "../../context/HomeContext";
+import { useNavigate } from "react-router-dom";
+// import { HomeContext } from "../../context/HomeContext";
 import ReservationCard from "../../components/ReservationCard";
 import PropertyListing from "../../components/PropertyListing";
 import calendar_green from "../../assets/icons/calendar-green-dark.svg";
@@ -11,12 +11,11 @@ import home_green from "../../assets/icons/home-green-dark.svg";
 import plus_white from "../../assets/icons/plus-circle-white.svg";
 
 const Dashboard = () => {
-
     const [reservations, setReservation] = useState([]);
     const [loggedIn, setLoggedIn] = useState(true);
     const [filterState, setFilterState] = useState({
-      role: "all",
-      state: "all"
+        role: "all",
+        state: "all",
     });
     const navigate = useNavigate();
     const [pageCount, setPageCount] = useState(0);
@@ -24,82 +23,84 @@ const Dashboard = () => {
     const [properties, setProperties] = useState([]);
     // const { propcards, setPropcards } = useContext(HomeContext);
 
-
-    const [count, setCount] = useState(0);
-    const [cancelation, setCancelation] = useState(0);
+    // const [count, setCount] = useState(0);
+    // const [cancelation, setCancelation] = useState(0);
+    const access_token = localStorage.getItem("accessToken");
 
     useEffect(() => {
         async function fetchReservation() {
-        //   const access_token = localStorage.getItem('access_token');
-          const access_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjgyMjk0MjkzLCJpYXQiOjE2ODIyMDc4OTMsImp0aSI6IjlhYjhkZWY5MWEyMjQ3OTFhMmM4OTM5Yjc1NGYyMjY4IiwidXNlcl9pZCI6NH0.1I7zQFuv6dArNCvYRlRl9PfgiTWRG_qEXjWDMFtP024";
-          if (access_token) {
-            try {
-            const headers = {
-              Authorization: `Bearer ${access_token}`,
-            };
-            const { role, state } = filterState;
-            let url = `http://localhost:8000/api/reservation/?page=${currentPage}&type=guest`
-            if (state !== "all") {
-              url += `&status=${state.substring(0, 2).toUpperCase()}`;
+            if (access_token) {
+                try {
+                    const headers = {
+                        Authorization: `Bearer ${access_token}`,
+                    };
+                    const { role, state } = filterState;
+                    let url = `http://localhost:8000/api/reservation/?page=${currentPage}&type=guest`;
+                    if (state !== "all") {
+                        url += `&status=${state.substring(0, 2).toUpperCase()}`;
+                    }
+                    const response = await fetch(url, { headers });
+                    // const response = await axios.get(url, { headers });
+
+                    const reservationPromises = (await response.json()).results.map(async (reservation) => {
+                        const propertyResponse = await fetch(
+                            `http://localhost:8000/api/property/${reservation.property_id}`
+                        );
+                        return {
+                            ...reservation,
+                            property: await propertyResponse.json(),
+                        };
+                    });
+                    const resolvedReservations = await Promise.all(reservationPromises);
+                    setReservation(resolvedReservations);
+                } catch (error) {
+                    console.error("Error fetching reservations:", error);
+                }
+            } else {
+                navigate("/auth");
             }
-            // const response = await fetch(url, { headers });
-            const response = await axios.get(url, { headers });
-
-            const reservationPromises = response.data.results.map(async (reservation) => {
-                const propertyResponse = await axios.get(`http://localhost:8000/api/property/${reservation.property_id}`);
-              return {
-                  ...reservation,
-                  property: propertyResponse.data,
-              };
-          });
-          const resolvedReservations = await Promise.all(reservationPromises);
-          setReservation(resolvedReservations);}
-          catch (error){
-            console.error("Error fetching reservations:", error);
-          }
-          }
-        };
+        }
         fetchReservation();
-      }, [currentPage]);
+    }, [currentPage, access_token, navigate]);
 
-      useEffect(() => {
+    useEffect(() => {
         async function fetchProperties() {
-        //   const access_token = localStorage.getItem('access_token');
-          const access_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjgyMjk0MjkzLCJpYXQiOjE2ODIyMDc4OTMsImp0aSI6IjlhYjhkZWY5MWEyMjQ3OTFhMmM4OTM5Yjc1NGYyMjY4IiwidXNlcl9pZCI6NH0.1I7zQFuv6dArNCvYRlRl9PfgiTWRG_qEXjWDMFtP024";
-          if (access_token) {
-            try {
-            const headers = {
-              Authorization: `Bearer ${access_token}`,
-            };
-            let url = `http://localhost:8000/api/user/profile/`;
-            // const response = await fetch(url, { headers });
-            const response = await axios.get(url, { headers });
+            if (access_token) {
+                try {
+                    const headers = {
+                        Authorization: `Bearer ${access_token}`,
+                    };
+                    let url = `http://localhost:8000/api/user/profile/`;
+                    const response = await fetch(url, { headers });
+                    // const response = await axios.get(url, { headers });
 
-            const propertyResponse = await axios.get(`http://localhost:8000/api/property/?host_id=${response.data.id}`);
-            // console.log(propertyResponse.data.results);
-          const resolvedProperties = await Promise.all(propertyResponse.data.results);
-          setProperties(resolvedProperties);}
-          catch (error){
-            console.error("Error fetching reservations:", error);
-          }
-          }
-        };
+                    const propertyResponse = await fetch(
+                        `http://localhost:8000/api/property/?host_id=${(await response.json()).id}`
+                    );
+                    // console.log(propertyResponse.data.results);
+                    const resolvedProperties = await Promise.all((await propertyResponse.json()).results);
+                    setProperties(resolvedProperties);
+                } catch (error) {
+                    console.error("Error fetching reservations:", error);
+                }
+            } else {
+                navigate("/auth");
+            }
+        }
         fetchProperties();
-      }, [currentPage]);
+    }, [currentPage, access_token, navigate]);
 
-      const handleFilterChange = (e) => {
+    const handleFilterChange = (e) => {
         // const { name, value } = e.target;
-        const {value} = e.target;
+        const { value } = e.target;
 
-        setFilterState((prevState) => ({ ...prevState, state: value}));
+        setFilterState((prevState) => ({ ...prevState, state: value }));
         // setFilterState((prevState) => ({ ...prevState, [name]: value }));
         handleFilterSubmit(e);
-      };
+    };
 
-      const handleFilterSubmit = async (e) => {
+    const handleFilterSubmit = async (e) => {
         e.preventDefault();
-        const access_token = localStorage.getItem("access_token");
-      
         const { role, state } = filterState;
         let url = "http://localhost:8000/api/reservation/?page=1&type=guest";
         setCurrentPage(1);
@@ -107,90 +108,101 @@ const Dashboard = () => {
         //   url += "&type=guest";
         // }
         if (state !== "all") {
-          url += `&status=${state.substring(0, 2).toUpperCase()}`;
+            url += `&status=${state.substring(0, 2).toUpperCase()}`;
         }
-    
+
         const response = await fetch(url, {
-          headers: {
-            Authorization: `Bearer ${access_token}`,
-          },
+            headers: {
+                Authorization: `Bearer ${access_token}`,
+            },
         });
         const data = await response.json();
         setPageCount(Math.ceil(data.count / 5));
         setReservation(data);
-      };
+    };
 
-      const handlePageClick = (e) => {
-        const newPage = Number(e.target.textContent);
-        setCurrentPage(newPage);
-      }
+    // const handlePageClick = (e) => {
+    //     const newPage = Number(e.target.textContent);
+    //     setCurrentPage(newPage);
+    // };
 
     return (
-    <>
-        <Navbar />
-        <div className="container-dash">
-            <section className="section-user">
-                <header className="section-header">
+        <>
+            <Navbar />
+            <div className="container-dash">
+                <section className="section-user">
+                    <header className="section-header">
+                        <span className="section-header-title">
+                            <img className="icon" src={calendar_green} alt="Reservation" />
+                            <h3 className="reservations_tag">Your reservations (User)</h3>
+                        </span>
 
-                    <span className="section-header-title">
-                        <img className="icon" src={calendar_green} alt="Reservation" />
-                        <h3 className="reservations_tag">Your reservations (User)</h3>
-                    </span>
+                        <label>
+                            Status:
+                            <select name="state" value={filterState.state} onChange={handleFilterChange}>
+                                <option value="all" onClick={handleFilterChange}>
+                                    All
+                                </option>
+                                <option value="Pending" onClick={handleFilterChange}>
+                                    Pending
+                                </option>
+                                <option value="Denied" onClick={handleFilterChange}>
+                                    Denied
+                                </option>
+                                <option value="Expired" onClick={handleFilterChange}>
+                                    Expired
+                                </option>
+                                <option value="Approved" onClick={handleFilterChange}>
+                                    Approved
+                                </option>
+                                <option value="Completed" onClick={handleFilterChange}>
+                                    Completed
+                                </option>
+                                <option value="Canceled" onClick={handleFilterChange}>
+                                    Canceled
+                                </option>
+                                <option value="Terminated" onClick={handleFilterChange}>
+                                    Terminated
+                                </option>
+                            </select>
+                        </label>
+                    </header>
 
-                    <label>
-                        Status:
-                        <select name="state" value={filterState.state} onChange={handleFilterChange}>
-                            <option value="all" onClick={handleFilterChange}>All</option>
-                            <option value="Pending" onClick={handleFilterChange}>Pending</option>
-                            <option value="Denied" onClick={handleFilterChange}>Denied</option>
-                            <option value="Expired" onClick={handleFilterChange}>Expired</option>
-                            <option value="Approved" onClick={handleFilterChange}>Approved</option>
-                            <option value="Completed" onClick={handleFilterChange}>Completed</option>
-                            <option value="Canceled" onClick={handleFilterChange}>Canceled</option>
-                            <option value="Terminated" onClick={handleFilterChange}>Terminated</option>
-                        </select>
-                    </label>
-                </header>
-
-
-                {loggedIn ? (
-                <div className="carousel">
-                    <div className="carousel-cards">
-                        {reservations.map((reservation) => (
-                            <ReservationCard reservation={reservation} />
-                        ))}
-
-                    </div>
-                </div>) : (
-                <h3> Not logged in</h3>)}
-
-
-            </section>
-
-            <section className="section-host">
-                        <header className="section-header">
-                            <span className="section-header-title">
-                                <img className="icon" src={home_green} alt="House" />
-                                <h3 className="listings_tag">Your listed properties (Host)</h3>
-                            </span>
-                            <button className="action-btn purple-dark">
-                                <img src={plus_white} alt="+" />
-                                Add rental property
-                            </button>
-                        </header>
+                    {loggedIn ? (
                         <div className="carousel">
                             <div className="carousel-cards">
-                                {properties.map((property) => (
-                                    <PropertyListing property={property} />
+                                {reservations.map((reservation) => (
+                                    <ReservationCard reservation={reservation} />
                                 ))}
-
                             </div>
                         </div>
-            </section>
-        </div>
+                    ) : (
+                        <h3> Not logged in</h3>
+                    )}
+                </section>
 
-    </>
-);
-}
+                <section className="section-host">
+                    <header className="section-header">
+                        <span className="section-header-title">
+                            <img className="icon" src={home_green} alt="House" />
+                            <h3 className="listings_tag">Your listed properties (Host)</h3>
+                        </span>
+                        <button className="action-btn purple-dark">
+                            <img src={plus_white} alt="+" />
+                            Add rental property
+                        </button>
+                    </header>
+                    <div className="carousel">
+                        <div className="carousel-cards">
+                            {properties.map((property) => (
+                                <PropertyListing property={property} />
+                            ))}
+                        </div>
+                    </div>
+                </section>
+            </div>
+        </>
+    );
+};
 
 export default Dashboard;
--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -2,165 +2,151 @@ import React, { useState, useEffect, useContext } from "react";
 import "./home.css";
 import Navbar from "../../components/Navbar";
 import PropertyListing from "../../components/PropertyListing";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 import { HomeContext } from "../../context/HomeContext";
 import Sidebar from "../../components/Sidebar";
-import InfiniteScroll from 'react-infinite-scroll-component'
-
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-import SortBtn from "../../components/SortBtn";
-import { set } from "mongoose";
-
 
 const Home = () => {
-    const [address, setAddress] = useState('');
+    const [address, setAddress] = useState("");
     const [guestCapacity, setGuestCapacity] = useState(1);
-    const [amenities, setAmenities] = useState('');
-    const [from, setFrom] = useState('');
-    const [to, setTo] = useState('');
+    const [amenities, setAmenities] = useState("");
+    const [from, setFrom] = useState("");
+    const [to, setTo] = useState("");
     const [propNext, setPropNext] = useState(null);
-  
-    const [sort, setSort] = useState('');
-    const [order, setOrder] = useState('');
-  
+
+    const [sort, setSort] = useState("");
+    const [order, setOrder] = useState("");
+
     const [page, setPage] = useState(1);
-    const [pageA, setPageA] = useState(1);
+    // const [pageA, setPageA] = useState(1);
     const [count, setCount] = useState(0);
-  
+
     const { propcards, setPropcards } = useContext(HomeContext);
     const navigate = useNavigate();
 
     useEffect(() => {
         fetchProperties();
-      }, [sort, order]);
+    }, [sort, order]);
 
-      useEffect(() => {
+    useEffect(() => {
         fetchProperties1();
-      }, [page]);
-
-
+    }, [page]);
 
     const fetchProperties = async () => {
         try {
-          // Construct the final URL with query parameters
-          let finalUrl = `http://localhost:8000/api/property/?page=${page}`;
-          const queryParams = [];
-    
-          if (address) {
-            queryParams.push(`location=${address}`);
-          }
-          if (guestCapacity) {
-            queryParams.push(`num_guests=${guestCapacity}`);
-          }
-          if (amenities) {
-            queryParams.push(`amenities=${amenities}`);
-          }
-          if (from) {
-            queryParams.push(`from=${from}`);
-          }
-          if (to) {
-            queryParams.push(`to=${to}`);
-          }
-          if (sort && order) {
-            if (sort === 'earliest_availability' || sort === 'rating') {
-                if (order === 'asc') {
-                    queryParams.push(`ordering=${sort}`);
-                }
-                else {
-                    queryParams.push(`ordering=-${sort}`);  
+            // Construct the final URL with query parameters
+            let finalUrl = `http://localhost:8000/api/property/?page=${page}`;
+            const queryParams = [];
+
+            if (address) {
+                queryParams.push(`location=${address}`);
+            }
+            if (guestCapacity) {
+                queryParams.push(`num_guests=${guestCapacity}`);
+            }
+            if (amenities) {
+                queryParams.push(`amenities=${amenities}`);
+            }
+            if (from) {
+                queryParams.push(`from=${from}`);
+            }
+            if (to) {
+                queryParams.push(`to=${to}`);
+            }
+            if (sort && order) {
+                if (sort === "earliest_availability" || sort === "rating") {
+                    if (order === "asc") {
+                        queryParams.push(`ordering=${sort}`);
+                    } else {
+                        queryParams.push(`ordering=-${sort}`);
+                    }
                 }
             }
-          }
 
-          if (queryParams.length > 0) {
-            finalUrl += `&${queryParams.join('&')}`;
-          }
-    
-          const response = await fetch(finalUrl);
-          const data = await response.json();
-          setPropNext(data.next);
-          setPropcards(data.results);
-          // setPropcards([...propcards, ...data.results]);
-          setCount(data.count);
-    
+            if (queryParams.length > 0) {
+                finalUrl += `&${queryParams.join("&")}`;
+            }
+
+            const response = await fetch(finalUrl);
+            const data = await response.json();
+            setPropNext(data.next);
+            setPropcards(data.results);
+            // setPropcards([...propcards, ...data.results]);
+            setCount(data.count);
         } catch (error) {
-          console.error('Error fetching properties:', error);
+            console.error("Error fetching properties:", error);
         }
-    }
+    };
 
     const fetchProperties1 = async () => {
-      try {
-        // Construct the final URL with query parameters
-        let finalUrl = `http://localhost:8000/api/property/?page=${page}`;
-        const queryParams = [];
-  
-        if (address) {
-          queryParams.push(`location=${address}`);
-        }
-        if (guestCapacity) {
-          queryParams.push(`num_guests=${guestCapacity}`);
-        }
-        if (amenities) {
-          queryParams.push(`amenities=${amenities}`);
-        }
-        if (from) {
-          queryParams.push(`from=${from}`);
-        }
-        if (to) {
-          queryParams.push(`to=${to}`);
-        }
-        if (sort && order) {
-          if (sort === 'earliest_availability' || sort === 'rating') {
-              if (order === 'asc') {
-                  queryParams.push(`ordering=${sort}`);
-              }
-              else {
-                  queryParams.push(`ordering=-${sort}`);  
-              }
-          }
-        }
+        try {
+            // Construct the final URL with query parameters
+            let finalUrl = `http://localhost:8000/api/property/?page=${page}`;
+            const queryParams = [];
 
-        if (queryParams.length > 0) {
-          finalUrl += `&${queryParams.join('&')}`;
-        }
-  
-        const response = await fetch(finalUrl);
-        const data = await response.json();
-        setPropNext(data.next);
-        // setPropcards(data.results);
-        setPropcards([...propcards, ...data.results]);
-        setCount(data.count);
-  
-      } catch (error) {
-        console.error('Error fetching properties:', error);
-      }
-  }
+            if (address) {
+                queryParams.push(`location=${address}`);
+            }
+            if (guestCapacity) {
+                queryParams.push(`num_guests=${guestCapacity}`);
+            }
+            if (amenities) {
+                queryParams.push(`amenities=${amenities}`);
+            }
+            if (from) {
+                queryParams.push(`from=${from}`);
+            }
+            if (to) {
+                queryParams.push(`to=${to}`);
+            }
+            if (sort && order) {
+                if (sort === "earliest_availability" || sort === "rating") {
+                    if (order === "asc") {
+                        queryParams.push(`ordering=${sort}`);
+                    } else {
+                        queryParams.push(`ordering=-${sort}`);
+                    }
+                }
+            }
 
-    const handlePageClick = (e) => {
-        const newPage = Number(e.target.textContent);
-        setPage(newPage);
-      };
+            if (queryParams.length > 0) {
+                finalUrl += `&${queryParams.join("&")}`;
+            }
+
+            const response = await fetch(finalUrl);
+            const data = await response.json();
+            setPropNext(data.next);
+            // setPropcards(data.results);
+            setPropcards([...propcards, ...data.results]);
+            setCount(data.count);
+        } catch (error) {
+            console.error("Error fetching properties:", error);
+        }
+    };
+
+    // const handlePageClick = (e) => {
+    //     const newPage = Number(e.target.textContent);
+    //     setPage(newPage);
+    // };
 
     const handleSearch = () => {
         setPage(1);
         fetchProperties();
         // handleScroll();
-      };
+    };
 
     const handleSort = (sortType, sortOrder) => {
         setSort(sortType);
         setOrder(sortOrder);
         setPage(1);
-      };
+    };
 
-      const handleScroll = () => {
-        if (propNext) {
-          setPageA(page + 1);
-          fetchProperties1();
-        }
-      };
+    // const handleScroll = () => {
+    //     if (propNext) {
+    //         setPageA(page + 1);
+    //         fetchProperties1();
+    //     }
+    // };
 
     // const handleReset = () => {
     //     setAddress('');
@@ -174,27 +160,37 @@ const Home = () => {
     //     fetchProperties();
     //   };
 
-    return (<>
-        <Navbar />
-        <div className="container">
-            <Sidebar location={address} setAddress={setAddress} 
-            from={from} setFrom={setFrom} 
-            to={to} setTo={setTo}
-            guestCapacity={guestCapacity} setGuestCapacity={setGuestCapacity}
-            amenities={amenities} setAmenities={setAmenities}
-            click={handleSearch} click1={handleSort} />
+    return (
+        <>
+            <Navbar />
+            <div className="container">
+                <Sidebar
+                    location={address}
+                    setAddress={setAddress}
+                    from={from}
+                    setFrom={setFrom}
+                    to={to}
+                    setTo={setTo}
+                    guestCapacity={guestCapacity}
+                    setGuestCapacity={setGuestCapacity}
+                    amenities={amenities}
+                    setAmenities={setAmenities}
+                    click={handleSearch}
+                    click1={handleSort}
+                />
 
-            <div className="home-content home-cards">
-
-                {propcards.map((property) => (
-                    <PropertyListing property={property}
-                    onClick={() => {
-                        //Open Reservation Model
-                        navigate(`/property/${property.id}`);
-                    }}
-                    />
-                ))}
-{/* 
+                <div className="home-content home-cards">
+                    {propcards.map((property, i) => (
+                        <PropertyListing
+                            key={i}
+                            property={property}
+                            onClick={() => {
+                                //Open Reservation Model
+                                navigate(`/property/${property.id}`);
+                            }}
+                        />
+                    ))}
+                    {/* 
                     <InfiniteScroll
                         next={() => setPage(page + 1)}
                         hasMore={(propNext != null)}
@@ -206,14 +202,13 @@ const Home = () => {
 
                     </InfiniteScroll> */}
 
-
-                  {/* </Row>
+                    {/* </Row>
               </Container> */}
+                </div>
             </div>
-        </div>
-        {/* <h1>Home</h1> */}
-    </>);
-
+            {/* <h1>Home</h1> */}
+        </>
+    );
 };
 
 export default Home;
