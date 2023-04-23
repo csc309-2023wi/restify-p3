c839c2e059e5ace1a366d3d76fb19d38215d060d
aryanbagga <aryanbagga8@gmail.com>
2023-04-23 13:02:03 -0400

    also fixed the location input looking weird

--- frontend/src/components/LocationInput/index.jsx
+++ frontend/src/components/LocationInput/index.jsx
@@ -6,7 +6,7 @@ const LocationInput = ({ onChangeHandler }) => {
     return (
         <>
             <button className="btn-map"></button>
-            <input type="text" id="location" className="rounded-r" onChange={onChangeHandler} />
+            <input type="text" id="location" className="rounded-r-loc" onChange={onChangeHandler} />
         </>
     );
 }
